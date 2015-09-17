a89498a360b857a8ea6f8814a817efaed3312027
Sam Eubank <sameubank+zerious@gmail.com>
2015-09-17 08:24:20 -0700

    Initial commit

---
+++ test/snippet-stack-bench.js
@@ -0,0 +1,16 @@
+var fs = require('fs')
+var snippet-stack = require(__dirname + '/lighter-snippet-stack')
+
+describe('Benchmarks', function () {
+
+  bench('', function () {
+
+    it('', function () {
+    })
+
+    it('', function () {
+    })
+
+  })
+
+})
---
+++ test/snippet-stack-test.js
@@ -0,0 +1,26 @@
+var snippet-stack = require('../lighter-snippet-stack')
+var is = global.is || require('exam/lib/is')
+
+describe('snippet-stack', function () {
+
+  describe('', function () {
+
+    it('', function () {
+    })
+
+    it('', function () {
+    })
+
+  })
+
+  describe('', function () {
+
+    it('', function () {
+    })
+
+    it('', function () {
+    })
+
+  })
+
+})
